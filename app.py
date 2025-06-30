fdfa9aef4638b435fbc49305ee00a1ac050d1a2c
loew-tech <loew.technology@gmail.com>
2025-06-29 18:39:38 -0600

    some work

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+from typing import List, Dict, Tuple
+
 from flask import Flask
 from flask_cors import CORS
 
@@ -5,10 +8,44 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+with open('db.json') as in_:
+    db = json.load(in_)['categories']
 
-@app.route('/')
+
+@app.get('/')
 def index():
     return '<div>Hello World</div>'
+
+
+@app.get('/categories')
+def categories() -> List[str]:
+    return [*db.keys()]
+
+
+@app.get('/categories/<string:category>')
+def get_category(category: str) -> List[Dict[str, str]]:
+    return db[category]
+
+
+@app.get('/categories/<string:category>/pick')
+def pick() -> str:
+    return NotImplemented
+
+
+@app.get('/categories/<string:category>/remove/<string:name>')
+def remove(category, name: str) -> Tuple[Dict[str, Dict[str, str]], int]:
+    name = name.replace('+', ' ')
+    print(f'{category=} {name=}')
+    if category not in db or name not in (indices:={d['name']: i for i, d
+                                          in enumerate(db[category])}):
+        return db, 404
+    del db[category][indices[name]]
+    if not db[category]:
+        del db[category]
+
+    with open('db.json', 'w') as out:
+        out.write(json.dumps({'categories': db}, indent=4))
+    return db, 202
 
 
 if __name__ == '__main__':
